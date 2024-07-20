e61ecd0160c509024d184204fc439e3703e58f20
zDany01 <85403430+zDany01@users.noreply.github.com>
2024-07-20 20:52:05 +0200

    Fixed bot commands and added bash managing script

--- bot.py
+++ bot.py
@@ -16,21 +16,33 @@ class ProcessOutput:
         self.output = output
         self.good = exitcode == 0
 
-def executeCommand(path: str, args: str = "", errormsg = "") -> ProcessOutput:
+def executeCommand(path: str, args = [], errormsg = "") -> ProcessOutput:
     tlock.acquire()
+    command = []
+    command.append(path)
+    for x in args:
+        command.append(x)
+    print("Executing " + path + " with args: " + " ".join(args))
     try:
         global exitcode
         global output
-        process = Popen(path + (' ' + args if args != "" else ""), stdout=PIPE)
+        #process = Popen(path + (' ' + args if args != "" else ""), stdout=PIPE)
+        process = Popen(command, stdout=PIPE)
         (output, err) = process.communicate()
         exitcode = process.wait()
-    except:
+    except ValueError as err2:
         exitcode = -1
+        print("[PYTHON ERROR] - " + err2.decode("utf-8"))
     finally:
         tlock.release()
     if(exitcode != 0):
         if (errormsg != ""):
             bot.send_message(CHAT_ID, errormsg)
+        print("Executing command: ",end='')
+        for part in command:
+            print('|' + part + '|', end=' ')
+        print()
+        print("Generated this error:" + output.decode("utf-8"))
         return ProcessOutput(exitcode, output.decode("utf-8"))
     return ProcessOutput(exitcode, output.decode("utf-8"))
 
@@ -76,16 +88,23 @@ class Commands:
     def live(self, message: Message):
         if(AuthCheck(message.chat.id)):
             bot.send_message(message.chat.id, "Safe and Sound")
-    #def redocker(self, message: Message):
-    #    if(AuthCheck(message.chat.id)):
-    #        if(executeCommand("bash", "-c \"docker restart $(docker ps -a -q)\"", "Unable to restart docker containers..").good):
-    #            bot.send_message(message.chat.id, "All containers successfully restarted")
-    #
-    #def showsvc(self, message: Message):
-    #    if(AuthCheck(message.chat.id)):
-    #        processOut = executeCommand("bash", "-c \"docker ps --format \"{{{{.Names}}}} -> {{{{.Status}}}}\"", "Unable to access docker containers")
-    #        if processOut.good:
-    #            bot.send_message(message.chat.id, processOut.output)
+    def redocker(self, message: Message):
+        if(AuthCheck(message.chat.id)):
+            containerlistprc = executeCommand("docker", ["ps", "-a", "-q"], "Unable to get container list")
+            if not containerlistprc.good:
+                return
+            containerlist = containerlistprc.output.splitlines()
+            print(containerlist)
+            restarted = True
+            for container in containerlist:
+                restarted = restarted & executeCommand("docker", ["restart", container], "Unable to restart" + container).good
+            bot.send_message(message.chat.id, "All containers restarted" if restarted else "Not all container were restarted")
+    
+    def showsvc(self, message: Message):
+        if(AuthCheck(message.chat.id)):
+            processOut = executeCommand("docker", ["ps", "--format", "{{.Names}} -> {{.Status}}"], "Unable to access docker containers")
+            if processOut.good:
+                bot.send_message(message.chat.id, processOut.output)
 
 bot.start()
 bot.add_commands(Commands(bot))
