105f20ae85fee4e1184bdc589a38f5180b2dfe5d
zDany01 <85403430+zDany01@users.noreply.github.com>
2024-10-08 00:05:59 +0200

    Improved CodeMessage class
    Added
     - bind function to link to already existing messages
    
    Improved
     - create and send function to include the reply_markup

--- bot.py
+++ bot.py
@@ -52,13 +52,17 @@ class CodeMessage:
     def append(self, text: str):
         self.message += text
         return self
-    
-    def create(self, chatID: int):
-        self.messageObject = bot.send_message(chatID, "```{0}\n{1}```".format(self.caption, self.message), "MarkdownV2")
-    
-    def send(self):
-        self.messageObject = bot.edit_message_text(self.messageObject.chat.id, "```{0}\n{1}```".format(self.caption, self.message), self.messageObject.message_id, parse_mode="MarkdownV2")
-    
+
+    def bind(self, message: Message):
+        self.messageObject = message
+        return self
+
+    def create(self, chatID: int, replyMarkup: ReplyMarkup = None):
+        self.messageObject = bot.send_message(chatID, "```{0}\n{1}```".format(self.caption, self.message), "MarkdownV2", reply_markup=replyMarkup)
+
+    def send(self, replyMarkup: ReplyMarkup = None):
+        self.messageObject = bot.edit_message_text(self.messageObject.chat.id, "```{0}\n{1}```".format(self.caption, self.message), self.messageObject.message_id, parse_mode="MarkdownV2", reply_markup=replyMarkup)
+
     def clear(self):
         self.message = ""
 
